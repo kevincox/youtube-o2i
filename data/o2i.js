29c39aee0c5984b1f9b809ebddbb0803288b25b7
Kevin Cox <kevincox.ca@gmail.com>
2012-11-28 19:12:19 -0500

    Added workaround for youtube bug.

--- data/o2i.js
+++ data/o2i.js
@@ -7,7 +7,6 @@ for ( i in objectsel )
 
 for ( i in objects )
 {
-	console.log(i);
 	var o = objects[i];
 	if ( o.tagName != "OBJECT" ) continue; // We are also getting properties.
 	var e = o.getElementsByTagName("embed")[0];
@@ -16,7 +15,18 @@ for ( i in objects )
 	if (r)
 	{
 		let i = document.createElement("iframe");
-		i.src = e.src.replace("/v/", "/embed/"); // Turn the flash url into an embed url.
+
+		//i.src = e.src.replace("/v/", "/embed/"); // Turn the flash url into an embed url.
+
+		/* So the above line **should** work but when the video is embeded
+		   without any parameters, youtube adds some incorrectaly which makes
+		   the iframe embed 404.  So this checks to see if the URL is proper and
+		   fixes it if it isn't.
+		*/
+		var src = e.src.replace("/v/", "/embed/");
+		if ( src.indexOf("?") < 0 ) src = src.replace(/&/, "?"); // Not global.
+		i.src = src;
+
 		i.style.width  = o.width  + "px";
 		i.style.height = o.height + "px";
 
